1be7e71288d51523813d966f7a7507d6244a34c4
longlee1107 <59003423+longlee1107@users.noreply.github.com>
2022-04-26 08:28:25 +0700

    update code

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -32,26 +32,39 @@ const Signup = () => {
         }
 
     };
+    const _goToLogin = () => {
+        navigate('/login')
+    }
 
 
     return (
-        <div className="container mx-auto">
-            <form action="" className="px-auto pt-6 pb-8 mb-4 bg-white rounded" onSubmit={formPrevent}>
-                <div className="">
-                    <h1 className="font-bold text-2xl mb-7">Sign up</h1>
-                    <hr className="border-1 border-solid border-slate-100" />
-                    <label htmlFor="email" className="font-bold">Email</label>
-                    <input type="text" className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline focus:text-black focus:bg-zinc-300" id="email"
-                        onChange={(evt) => setEmail(evt.target.value)} placeholder="Email" value={email} />
-                    <label htmlFor="password" className="font-bold">Password</label>
-                    <input type="password" className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline focus:text-black focus:bg-zinc-300" id="password"
-                        onChange={(evt) => setPassword(evt.target.value)} placeholder="Password" value={password} />
-                    <div className="mt-5">
-                        <button className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outlin"
-                            onClick={submitForm}>Sign up</button>
-                    </div>
+        <div className="container" id="container">
+            <div className="width-fit w-3/4 mx-auto px-3 py-3">
+                <div className="grid grid-cols-1">
+                    <form action="" className="px-auto pt-6  bg-white rounded" onSubmit={formPrevent}>
+                        <div className="">
+                            <h1 className="font-bold text-2xl mb-7">Sign Up</h1>
+                            <hr className="border-1 border-solid border-slate-100" />
+                            <label htmlFor="email" className="font-bold">Email</label>
+                            <input type="text" className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none bg-gray-300 focus:outline-none focus:shadow-outline focus:text-black focus:bg-zinc-300" id="email"
+                                onChange={(evt) => setEmail(evt.target.value)} placeholder="Email" value={email} />
+                            <label htmlFor="password" className="font-bold">Password</label>
+                            <input type="password" className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none bg-gray-300 focus:outline-none focus:shadow-outline focus:text-black focus:bg-zinc-300" id="password"
+                                onChange={(evt) => setPassword(evt.target.value)} placeholder="Password" value={password} />
+                            <div className="mt-5">
+                                <button className="w-1/3 mx-auto px-4 py-2 font-bold text-white bg-green-500 rounded-full hover:bg-green-700 focus:outline-none focus:shadow-outline hover:translate-x-3"
+                                    onClick={submitForm}>Sign Up</button>
+                            </div>
+                        </div>
+                        <p className="font-bold italic">or</p>
+                    </form>
                 </div>
-            </form>
+                <div className="">
+                <button className="w-1/3 mx-auto px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline hover:translate-x-3"
+                    onClick={_goToLogin}>Sign In</button>
+                </div>
+                <p className="font-bold italic">if you have an account.</p>
+            </div>
         </div>
     );
 }
